69e9dcb03bca580fc8ee7a78a596044be5228ad8
MacBook Air <macbookair@Agaseeyyys-MacBook-Air.local>
2024-05-14 15:36:47 +0800

    final changes

--- public/script.js
+++ public/script.js
@@ -69,3 +69,33 @@ const mobileNav = document.querySelector(".mobileMenu");
 mobileNavBtn.addEventListener('click', () => {
   mobileNav.classList.toggle('md:hidden');
 });
+
+function sendEmail(e){
+    e.preventDefault();
+    (function(){
+        emailjs.init({
+          publicKey: "fBrm2QucDoMpGt-qz",
+        });
+    })();
+    let params = { 
+        from_name : document.getElementById("name").value,
+        email_id : document.getElementById("email").value,
+        message : document.getElementById("message").value
+    }
+    if(params.email_id == ""){
+        alert("Please field all the requirements!");
+        return false;
+    }
+    
+    emailjs.send("service_zey2njp", "template_ov22a9y", params)
+    .then(function (res){
+        alert("Message Sent!");
+    })
+    .catch(function (error) {
+        console.error('Error sending email:', error);
+        alert('Failed to send message. Please try again.');
+      });
+}
+
+const contact = document.getElementById("contacts");
+contact.addEventListener('submit', sendEmail);
